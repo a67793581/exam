c2e2db440795912390b821ba8a2d87f20d82f6ae
carlo <284474102@qq.com>
2020-12-19 18:20:51 +0800

    初始化

--- app/web/router/api.go
+++ app/web/router/api.go
@@ -1,11 +1,13 @@
 package router
 
 import (
+	"exam/app/web/controller"
 	"github.com/gin-gonic/gin"
 )
 
 func Api(engine *gin.Engine) {
-	//api := engine.Group("/api")
-	//{
-	//}
+	api := engine.Group("/api")
+	{
+		api.GET("test", controller.Test)
+	}
 }
