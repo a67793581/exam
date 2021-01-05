78de65283a94ab9aa5f976a22c04535b6ef4da88
carlo <284474102@qq.com>
2021-01-05 16:32:24 +0800

    初始化

--- app/config/mysql.go
+++ app/config/mysql.go
@@ -1,8 +1,8 @@
 package config
 
-const MysqlUser = "go_blog"
+const MysqlUser = "exam_admin"
 const MysqlPassword = "67793581"
 const MysqlHost = "120.77.158.211"
 const MysqlPort = "3306"
-const MysqlDB = "go_blog"
-const MysqlCollation = "utf8mb4_general_ci"
+const MysqlDB = "exam"
+const MysqlCollation = "utf8mb4_unicode_ci"
