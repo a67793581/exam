d3d0c4d900c2fdad7df03e6277935223c8b62600
carlo <284474102@qq.com>
2021-01-15 18:03:48 +0800

    初始化

--- app/service/mysql/core.go
+++ app/service/mysql/core.go
@@ -1,1 +1,43 @@
 package mysql
+
+import (
+	"exam/app/config"
+	"fmt"
+	"gorm.io/driver/mysql"
+	"gorm.io/gorm"
+	"gorm.io/gorm/logger"
+	"gorm.io/gorm/schema"
+	"log"
+	"os"
+	"time"
+)
+
+func NewConnection() *gorm.DB {
+	newLogger := logger.New(
+		log.New(os.Stdout, "\r\n", log.LstdFlags), // io writer
+		logger.Config{
+			SlowThreshold: time.Second, // 慢 SQL 阈值
+			LogLevel:      logger.Info, // Log level
+			Colorful:      false,       // 禁用彩色打印
+		},
+	)
+	dsn := fmt.Sprintf("%s:%s@tcp(%s:%s)/%s?charset=%s&parseTime=true",
+		config.MysqlUser,
+		config.MysqlPassword,
+		config.MysqlHost,
+		config.MysqlPort,
+		config.MysqlDB,
+		config.MysqlCharset,
+	)
+
+	conn, err := gorm.Open(mysql.Open(dsn), &gorm.Config{
+		Logger: newLogger,
+		NamingStrategy: schema.NamingStrategy{
+			SingularTable: true, // 使用单数表名，启用该选项，此时，`User` 的表名应该是 `t_user`
+		},
+	})
+	if err != nil {
+		log.Print(err.Error())
+	}
+	return conn
+}
--- app/web/controller/test.go
+++ app/web/controller/test.go
@@ -1,17 +1,10 @@
 package controller
 
 import (
-	"exam/app/config"
 	"exam/app/model"
+	"exam/app/service/mysql"
 	"fmt"
 	"github.com/gin-gonic/gin"
-	"gorm.io/driver/mysql"
-	"gorm.io/gorm"
-	"gorm.io/gorm/logger"
-	"gorm.io/gorm/schema"
-	"log"
-	"os"
-	"time"
 )
 
 func Test(context *gin.Context) {
@@ -20,24 +13,7 @@ func Test(context *gin.Context) {
 }
 
 func TestMysql(context *gin.Context) {
-	dsn := config.MysqlUser + ":" + config.MysqlPassword + "@tcp(" + config.MysqlHost + ":" + config.MysqlPort + ")/" + config.MysqlDB + "?charset=" + config.MysqlCharset + "&parseTime=True&loc=Local"
-	newLogger := logger.New(
-		log.New(os.Stdout, "\r\n", log.LstdFlags), // io writer
-		logger.Config{
-			SlowThreshold: time.Second, // 慢 SQL 阈值
-			LogLevel:      logger.Info, // Log level
-			Colorful:      false,       // 禁用彩色打印
-		},
-	)
-	db, err := gorm.Open(mysql.Open(dsn), &gorm.Config{
-		Logger: newLogger,
-		NamingStrategy: schema.NamingStrategy{
-			SingularTable: true, // 使用单数表名，启用该选项，此时，`User` 的表名应该是 `t_user`
-		},
-	})
-	if err != nil {
-		panic(err)
-	}
+	db := mysql.NewConnection()
 	s := model.Student{Name: "carlo", Key: "test"}
 	db.Create(&s)
 	db.Delete(&s)
