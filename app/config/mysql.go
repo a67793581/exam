3b8d15c98e4f7f7d2bdeb3bffb88d15660017132
carlo <284474102@qq.com>
2021-02-24 13:39:54 +0800

    初始化

--- app/config/mysql.go
+++ app/config/mysql.go
@@ -29,11 +29,3 @@ func InsMysql() *mysql {
 	}
 	return insMysql
 }
-
-const MysqlUser = "exam_admin"
-const MysqlPassword = "67793581"
-const MysqlHost = "120.77.158.211"
-const MysqlPort = "3306"
-const MysqlDB = "exam"
-const MysqlCollation = "utf8mb4_unicode_ci"
-const MysqlCharset = "utf8mb4"
